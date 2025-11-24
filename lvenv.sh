fb953d4036ae06de8d9fbd790939634eb746c8b8
Tikhon-code <tihonbo4karyov@yandex.ru>
2025-11-24 16:36:46 +0700

    Add files via upload

--- lvenv.sh
+++ lvenv.sh
@@ -9,17 +9,25 @@ cd venv
 
 cat << EOF > activate
 [ "\$oldPROMPT" ] || oldPROMPT="\$PS1"
+[ "\$oldLUA_PATH" ] || oldLUA_PATH="\$LUA_PATH"
+[ "\$oldLUA_CPATH" ] || oldLUA_CPATH="\$LUA_CPATH"
 
 PS1="[lvenv] \$PS1"
 
 alias lua="$dir/venv/bin/lua"
 alias luac="$dir/venv/bin/luac"
 
+export LUA_PATH="${dir}/?.lua;${dir}/?/init.lua;;"
+export LUA_CPATH="${dir}/?.so;${dir}/?/loadall.so;;"
+
 deactivate() {
 	PS1="\$oldPROMPT"
 
 	unalias lua
 	unalias luac
+
+	export LUA_PATH="$oldLUA_PATH"
+	export LUA_CPATH="$oldLUA_CPATH"
 }
 EOF
 
